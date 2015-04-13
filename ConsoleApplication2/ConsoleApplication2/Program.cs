2c4f542cab79dd8eaa2c849c0a07c8c312c3951a
SatoshiMantani <satoshimantani@gmail.com>
2015-04-13 20:57:26 +0900

    多次元配列

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[, ]fibo=new int [25,4];
+            fibo[0, 0]=1;
+            fibo[0, 1]=0;
+            fibo[0, 2]=0;
+            fibo[0, 3]=1;
+
+           
+
+        }
+    }
+}
